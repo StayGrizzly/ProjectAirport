be765085d889ca7369a9e1fe2da47f4db57896de
ruben-farrus <rubenfarrus@gmail.com>
2016-03-17 14:51:35 -0400

    Uploaded the initial project

---
+++ ViveTestProject/Assets/Scripts/ControllerInputs.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ControllerInputs : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObj;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Start ()
+    {
+
+    }
+	
+
+	void Update ()
+    {
+            var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+            if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("Laser Down");
+                gameObject.GetComponent<SteamVR_LaserPointer>().thickness = .002f;
+            }
+            else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Debug.Log("Laser Up");
+                gameObject.GetComponent<SteamVR_LaserPointer>().thickness = 0;
+            }
+        }
+}
